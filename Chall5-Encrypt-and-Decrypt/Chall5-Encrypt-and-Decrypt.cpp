88da1bc2be4edf133f8452fc9e11a6188bb6bc9f
Jake Ralph <flooblegoo@gmail.com>
2016-05-11 16:05:13 +1000

    Inital Commit v0.1
    Code written but far from finished. Just basic structure for now and the
    idea behind it. Will work on and fix up.

---
+++ Chall5-Encrypt-and-Decrypt/Chall5-Encrypt-and-Decrypt.cpp
@@ -0,0 +1,60 @@
+// Challenge 5 - Basic XOR Encryption / Decryption
+// I will be trying out and learning a basic XOR encrypt/decrypt method here
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+
+std::string encryptDecrypt(std::string toEncrypt)
+{
+	char key = 'Y'; // Define the encryption key to use, will change this to an array of potential keys later
+	std::string output = toEncrypt;
+
+	for (int i = 0; i < toEncrypt.size(); i++)
+		output[i] = toEncrypt[i] ^ key;
+
+	return output;
+}
+
+int main()
+{
+	int menu_selection;
+	std::string decrypted;
+	std::string encrypted;
+	std::string user_entry_encrypt;
+	std::string user_entry_decrypt;
+
+	// Check if user wants to Encrypt or Decrypt a string using the preset key/s
+	std::cout << "1. Encrypt" << std::endl;
+	std::cout << "2. Decrypt" << std::endl;
+	std::cout << "\nSelection: ";
+	std::cin >> menu_selection;
+
+	switch (menu_selection)
+	{
+	case 1:
+		// Request string from the user that they wish to have encrypted
+		std::cout << "\nPlease enter the string to encrypt: ";
+		std::cin >> user_entry_encrypt;
+		// Perform the encryption and supply result
+		encrypted = encryptDecrypt(user_entry_encrypt);
+		std::cout << user_entry_encrypt << " encrypted with the preset key is " << encrypted << "\n";
+		break;
+	case 2:
+		// Request string from the user that they wish to have decrypted
+		std::cout << "\nPlease enter the string to decrypt: ";
+		std::cin >> user_entry_decrypt;
+		// Perform the encryption and supply result
+		decrypted = encryptDecrypt(user_entry_decrypt);
+		std::cout << user_entry_decrypt << " decrypted with the preset key is " << decrypted << "\n";
+		break;
+	default:
+		std::cout << "\nPlease enter 1 or 2 for your selection\n";
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		main();
+		break;
+	}
+
+	return 0;
+}
+
---
+++ Chall5-Encrypt-and-Decrypt/stdafx.cpp
@@ -0,0 +1,8 @@
+// stdafx.cpp : source file that includes just the standard includes
+// Chall5-Encrypt-and-Decrypt.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+
+// TODO: reference any additional headers you need in STDAFX.H
+// and not in this file
