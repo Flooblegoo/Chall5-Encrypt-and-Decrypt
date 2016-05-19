9923c1d7b2bbfca3e9bbd36add15e4168bf3dba4
Jake Ralph <flooblegoo@gmail.com>
2016-05-19 16:28:33 +1000

    V1.0
    Enabled the ability to utilise a key that the user defines. Haven't
    extensively bug-tested but it's a small program and it works.
    
    Only note to self: Must find an easier way to clear the input stream
    insteand of using std::cin.clear then std::cin.ignore(blahblah) every
    time I need to flush it. Maybe put it in a function?
    
    Anyway program is working, hard enough to break it and it does it's job
    as well as simplicity allows.

--- Chall5-Encrypt-and-Decrypt/Chall5-Encrypt-and-Decrypt.cpp
+++ Chall5-Encrypt-and-Decrypt/Chall5-Encrypt-and-Decrypt.cpp
@@ -7,7 +7,12 @@
 
 std::string encryptDecrypt(std::string toEncrypt)
 {
-	char key = 'Y'; // Define the encryption key to use, will change this to an array of potential keys later
+	char key; 
+
+	// Request key to be used in Encryption/Decryption
+	std::cout << "\nPlease enter the key you wish to use: ";
+	std::cin >> key;
+
 	std::string output = toEncrypt;
 
 	for (int i = 0; i < toEncrypt.size(); i++)
@@ -27,30 +32,49 @@ int main()
 	// Check if user wants to Encrypt or Decrypt a string using the preset key/s
 	std::cout << "1. Encrypt" << std::endl;
 	std::cout << "2. Decrypt" << std::endl;
+	std::cout << "3. Exit" << std::endl;
 	std::cout << "\nSelection: ";
 	std::cin >> menu_selection;
 
 	switch (menu_selection)
 	{
 	case 1:
+		std::cin.clear(); // Clear input stream 
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
 		// Request string from the user that they wish to have encrypted
 		std::cout << "\nPlease enter the string to encrypt: ";
-		std::cin >> user_entry_encrypt;
+		std::getline(std::cin, user_entry_encrypt);
 		// Perform the encryption and supply result
 		encrypted = encryptDecrypt(user_entry_encrypt);
-		std::cout << user_entry_encrypt << " encrypted with the preset key is " << encrypted << "\n";
+		std::cout << user_entry_encrypt << " encrypted with the preset key is " << encrypted << "\n\n";
+
+		std::cin.clear(); // Clear input stream and restart at menu
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); 
+		main();
 		break;
 	case 2:
+		std::cin.clear(); // Clear input stream 
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
 		// Request string from the user that they wish to have decrypted
 		std::cout << "\nPlease enter the string to decrypt: ";
-		std::cin >> user_entry_decrypt;
+		std::getline(std::cin, user_entry_decrypt);
 		// Perform the encryption and supply result
 		decrypted = encryptDecrypt(user_entry_decrypt);
-		std::cout << user_entry_decrypt << " decrypted with the preset key is " << decrypted << "\n";
+		std::cout << user_entry_decrypt << " decrypted with the preset key is " << decrypted << "\n\n";
+
+		std::cin.clear(); // Clear input stream and restart at menu
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); 
+		main();
+		break;
+	case 3:
 		break;
 	default:
-		std::cout << "\nPlease enter 1 or 2 for your selection\n" << std::endl;
-		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		std::cout << "\nPlease enter 1, 2 or 3 for your selection\n" << std::endl;
+
+		std::cin.clear(); // Clear input stream and restart at menu
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); 
 		main();
 		break;
 	}
